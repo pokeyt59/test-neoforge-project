5eda26d0bbbd2f4ba854c4d08cab2165c21b6d16
pokeyt59 <69089578+pokeyt59@users.noreply.github.com>
2024-07-19 22:56:02 -0500

    FINALLY GOT IT TO LOAD TO MENU 😁🙌

--- src/main/java/pokeyt59/github/com/pokey_test_id.java
+++ src/main/java/pokeyt59/github/com/pokey_test_id.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package pokeyt59.github.com;
 
 import org.slf4j.Logger;
 
@@ -35,11 +35,12 @@ import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(pokey_test_id.MODID)
+//   ^ to rename refactor file the change it in gradle.properties line 29
+public class pokey_test_id
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "examplemod";
+    public static final String MODID = "pokey_test_id";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
@@ -69,7 +70,7 @@ public class ExampleMod
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public ExampleMod(IEventBus modEventBus, ModContainer modContainer)
+    public pokey_test_id(IEventBus modEventBus, ModContainer modContainer)
     {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
